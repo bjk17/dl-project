b7e7258ce8e4c4e195234cb5d822c65cf6fe8f4a
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2018-12-06 14:23:14 +0000

    Fixing random seed to 13 in unittests for reproducibility

--- mcst.py
+++ mcst.py
@@ -8,7 +8,8 @@ def convert_result_string_to_value(result):
 
 
 class MCST:
-    def __init__(self, nnmodel, board_position, simulations=1):
+    def __init__(self, nnmodel, board_position, simulations=1, random_seed=None):
+        np.random.seed(random_seed)
         self.my_nn = nnmodel
         self.board = board_position
         self.simulations = simulations
@@ -17,9 +18,11 @@ class MCST:
     def get_position_estimate(self):
         return self.evaluation
 
-    def simulate_game_from_position(self, position):
-        board = copy.deepcopy(position)
+    def simulate_game_from_position(self, position, random_seed=None):
+        if random_seed:
+            np.random.seed(random_seed)
 
+        board = copy.deepcopy(position)
         while not board.is_game_over():
             mpv = self.my_nn.get_move_probabiliy_vector(board)
             move = np.random.choice(list(mpv.keys()), 1, replace=False, p=list(mpv.values()))[0]
@@ -29,9 +32,11 @@ class MCST:
         return convert_result_string_to_value(result)
 
     def simulate_games(self):
-        with concurrent.futures.ThreadPoolExecutor() as tpe:
+        # ThreadPoolExecutor() would result in race conditions to the np.random object
+        # resulting in non-deterministic outputs between simulations for fixed random seed
+        with concurrent.futures.ProcessPoolExecutor() as ppe:
             futures = list()
             for i in range(self.simulations):
-                futures.append(tpe.submit(self.simulate_game_from_position, self.board))
+                futures.append(ppe.submit(self.simulate_game_from_position, self.board, random_seed=i))
 
             return np.mean([future.result() for future in concurrent.futures.as_completed(futures)])
