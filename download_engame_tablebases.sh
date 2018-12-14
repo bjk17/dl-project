f1e813e8d6da00fc662578799f9f3a069aac89ff
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2018-12-14 09:15:20 +0000

    not redownloading tablebases when they already exist

--- download_engame_tablebases.sh
+++ download_engame_tablebases.sh
@@ -2,5 +2,6 @@
 
 mkdir -p tablebases/syzygy/regular
 for endgame in "KQvK"; do
-  wget http://tablebase.sesse.net/syzygy/3-4-5/${endgame}.{rtbw,rtbz} -P tablebases/syzygy/regular
+  wget -c http://tablebase.sesse.net/syzygy/3-4-5/${endgame}.{rtbw,rtbz} -P tablebases/syzygy/regular
 done
+9
